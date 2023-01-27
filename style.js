f74d31bcef59a41a8af7b924a06e8610a00f410d
bijudamian2108 <89126929+bijudamian2108@users.noreply.github.com>
2023-01-27 20:06:45 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,36 @@
+let currentPlayer = "X";
+let gameWon = false;
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach(cell => cell.addEventListener("click", handleClick));
+
+function handleClick(e) {
+  if (e.target.textContent !== "" || gameWon) return;
+  e.target.textContent = currentPlayer;
+  checkWin();
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+}
+
+function checkWin() {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (
+      cells[a].textContent === currentPlayer &&
+      cells[b].textContent === currentPlayer &&
+      cells[c].textContent === currentPlayer
+    ) {
+      gameWon = true;
+      alert(`Player ${currentPlayer} won!`);
+    }
+  }
+}
